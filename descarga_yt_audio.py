3ccec7e1c234b43a5cba94bebd8ca3572eb21445
Enrique Crespo <jecrespom@gmail.com>
2019-01-14 21:56:21 +0100

    Update descarga_yt_audio.py

--- descarga_yt_audio.py
+++ descarga_yt_audio.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 from pytube import YouTube
 yt = YouTube('www.youtube.com/watch?v=-la04e1zisU')
-yt.streams.filter(only_audio=True).first().download()
+yt.streams.filter(type='audio',subtype='mp4').first().download()
